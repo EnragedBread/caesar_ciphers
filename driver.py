4599cdf0e0e3635ba7ab07c8a776639175aa3c43
Bread <civisimulator@gmail.com>
2021-07-06 20:54:12 -0400

    make input handling more robust

--- driver.py
+++ driver.py
@@ -1,19 +1,27 @@
+from choose_from import choose_from_menu
 from cipher import encode, decode, brute_force
 
 def cipher():
-    message = input(f'\nWhat message would you like to encode/ decode? ').lower()
-    method = input(f'\nWould you like to decode, encode, or brute force your message? ').lower().strip()
-    shift = int(input(f'\nWhat number would you like to shift your message by? ').lower().strip())
+    choices = ['Encode', 'Decode', 'Brute Force']
 
-    if method == 'encode':
+    message = input(f'\nWhat message would you like to encode/ decode? ').lower()
+    method = choose_from_menu(choices, 'Would you like to Encode, Decode, or Brute Force your message? ')
+    while True:
+        try:
+            shift = int(input(f'\nWhat number would you like to shift your message by? ').strip())
+            break
+        except ValueError:
+            print(f'\nSorry, you may have inputed an invalid character!')
+
+    if method == 'Encode':
         message = encode(message, shift)
         print(f'\nYour shift was: {shift}, and your message is: {message}')
 
-    elif method == 'decode':
+    elif method == 'Decode':
         message = decode(message, shift)
         print(f'\nYour shift was: {shift}, and your message was: {message}')
 
-    elif method == 'brute force':
+    elif method == 'Brute Force':
         messages = brute_force(message)
         for shift,plaintext in messages:
             print(f'\nYour shift was: {shift}, and your message was: {plaintext}')
